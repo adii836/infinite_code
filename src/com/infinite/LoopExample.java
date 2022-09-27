7b1e5e958b26464c1df79ada286710a245f19fbd
adityara <adityarajssr@gmail.com>
2022-09-28 01:59:27 +0530

    classexample update

---
+++ src/com/infinite/LoopExample.java
@@ -0,0 +1,85 @@
+package com.infinite;
+
+import java.util.Scanner;
+
+public class LoopExample {
+
+    public static void main(String[] args ){
+
+
+        for(int i=0; i<10;i++){
+            System.out.println("hello " +i);
+        }
+
+        int k=0;
+        while (k<10){
+            System.out.println("learn new things ");
+            k++;
+        }
+
+        int i=0;
+        do {
+
+            System.out.println("learn do-while loops");
+            i++;
+        }while(i<10);
+
+        LoopExample l1 = new LoopExample();
+        l1.array();
+        l1.MultiDimensional();
+        l1.switchcase();
+
+    }
+
+    void array() {
+        int arr2[] = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.println("arr : " + arr2[i]);
+        }
+            for (int b : arr2) {
+                System.out.println("b = " + b);
+            }
+
+    }
+
+
+    void MultiDimensional() {
+        int arr[][] = {{1, 2, 3, 4},
+                {1, 5, 6},
+                {9, 8, 7, 6, 5}
+        };
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+
+                System.out.print("arr[" + i + "]" + "[" + j + "] : " + arr[i][j] + " ");
+            }
+            System.out.println("");
+        }
+
+
+        }
+
+    void switchcase(){
+        int ch;
+        Scanner sc = new Scanner(System.in);
+        ch= sc.nextInt();
+        switch (ch){
+            case 1:
+                System.out.println("case 1 :");
+                break;
+            case 2:
+                System.out.println("case 2 : ");
+                break;
+            case 3:
+                System.out.println("case 3 : ");
+                break;
+
+            default:
+                System.out.println("default");
+                break;
+        }
+
+    }
+}
