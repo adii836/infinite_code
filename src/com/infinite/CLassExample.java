e0c91f0f9a83b77b062422e0c104423dae1d79a6
adityara <adityarajssr@gmail.com>
2022-09-27 13:18:08 +0530

    classexample update

--- src/com/infinite/CLassExample.java
+++ src/com/infinite/CLassExample.java
@@ -1,10 +1,24 @@
 package com.infinite;
 
+
+
 public class CLassExample {
     int id;
     String name;
     String Contact_no;
     String Address;
+
+    public CLassExample(){
+        System.out.println("Normal cuntructor");
+    }
+
+    public CLassExample(int id,String name , String contact_no, String address){
+        this.id = id;
+        this.name = name;
+        this.Contact_no = contact_no;
+        this.Address = address;
+
+    }
 
     public int getId() {
         return id;
@@ -41,15 +55,9 @@ public class CLassExample {
     public static void main(String[] args) {
 
         CLassExample c1 = new CLassExample();
-        c1.setId(12);
-        c1.setName("aditya raj");
-        c1.setContact_no("8318874454");
-        c1.setAddress("Benglore");
+        c1.func();
 
-        System.out.println(c1.getId());
-        System.out.println(c1.getName());
-        System.out.println(c1.getContact_no());
-        System.out.println(c1.getAddress());
+
 
         CLassExample c2 = new CLassExample();
         c2.setId(13);
@@ -61,6 +69,51 @@ public class CLassExample {
         System.out.println(c2.getName());
         System.out.println(c2.getAddress());
         System.out.println(c2.getContact_no());
+
+        CLassExample c3 = new CLassExample();
+        System.out.println("id3: " + c3.getId());
+        System.out.println("name 3: " + c3.getName());
+        System.out.println(" address 3 :" + c3.getAddress());
+        System.out.println(" contact nno 3" +c3.getContact_no());
+
+
+
+    }
+    void func(){
+
+        CLassExample c1 = new CLassExample();
+
+        c1.setId(12);
+        c1.setName("aditya raj");
+        c1.setContact_no("8318874454");
+        c1.setAddress("Benglore");
+
+        System.out.println(c1.getId());
+        System.out.println(c1.getName());
+        System.out.println(c1.getContact_no());
+        System.out.println(c1.getAddress());
+
+        // and we can also do that
+
+       /* void func(){
+            Customer c1  =new Customer();
+            setData(c1);
+            getData(c1);
+        }
+        void setData(Customer c1){
+            c1.setName("harshada");
+            c1.setId(12);
+            c1.setContact_no("2354454");
+            c1.setAddress("mumbai");
+        }
+        void getData(Customer c1){
+            c1.display(6);
+            c1.display(4, 5);
+            c1.display(6, "hello");
+            c1.display(6, 6, "hello");
+            System.out.println(c1.getName());
+            System.out.println(c1.getId());
+        }*/
 
     }
 }
