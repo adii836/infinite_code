de98431a9d3a74c81aea7ad663ba6a47547a1da6
adityara <adityarajssr@gmail.com>
2022-10-06 22:57:22 +0530

    12 am update 5 oct

---
+++ src/code/InterfaceMamal.java
@@ -0,0 +1,31 @@
+package code;
+
+interface Mammal {
+    void run();
+}
+
+interface Cat extends Mammal {
+    void eat();
+}
+
+interface Lion extends Cat{}
+
+class Animal implements Lion {
+
+    public void run() {
+        System.out.println("Pouncing around");
+    }
+
+    public void eat() {
+        System.out.println("Too full to eat!");
+    }
+    // It can have more own methods.
+}
+
+public class InterfaceMamal {
+    public static void main(String[] args) {
+
+        Animal animal = new Animal();
+        animal.eat();
+        animal.run();
+}
